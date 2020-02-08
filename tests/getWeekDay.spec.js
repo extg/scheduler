7c92f1c10487b2b282d60e61f77a68069c27b2bb
Andrey Ermakov <a.ermakov@corp.vk.com>
2020-02-08 15:03:06 +0300

    migrate to TS

--- tests/getWeekDay.spec.js
+++ tests/getWeekDay.spec.js
@@ -2,7 +2,7 @@
 
 const tk = require('timekeeper');
 
-const {getWeekDay} = require('./utils');
+const { getWeekDay } = require('../src/utils');
 
 // Odd week
 // 5 Mon
@@ -33,45 +33,44 @@ const {getWeekDay} = require('./utils');
 
 describe('Сейчас четная неделя', () => {
   beforeEach(() => {
-    tk.travel(new Date('2018-11-12T00:00:00.000Z')) // 12 Oct, Mon, Even week
-  })
+    tk.travel(new Date('2018-11-12T00:00:00.000Z')); // 12 Oct, Mon, Even week
+  });
 
   afterAll(() => {
-    tk.reset()
-  })
+    tk.reset();
+  });
 
   it('нужен ближайший пн на четной (день в день)', () => {
-    expect(getWeekDay('Пн', 0).toString()).toEqual('Mon Nov 12 2018 03:00:00 GMT+0300')
-  })
+    expect(getWeekDay('Пн', 0).toString()).toEqual('Mon Nov 12 2018 03:00:00 GMT+0300');
+  });
 
   it('нужен ближайший пн на нечетной (день в день)', () => {
-    expect(getWeekDay('Пн', 1).toString()).toEqual('Mon Nov 19 2018 03:00:00 GMT+0300')
-  })
+    expect(getWeekDay('Пн', 1).toString()).toEqual('Mon Nov 19 2018 03:00:00 GMT+0300');
+  });
 
   it('нужен ближайший вт на четной (день после)', () => {
-    expect(getWeekDay('Вт', 0).toString()).toEqual('Tue Nov 13 2018 03:00:00 GMT+0300')
-  })
+    expect(getWeekDay('Вт', 0).toString()).toEqual('Tue Nov 13 2018 03:00:00 GMT+0300');
+  });
 
   it('нужен ближайший вт на нечетной (день после)', () => {
-    expect(getWeekDay('Вт', 1).toString()).toEqual('Tue Nov 20 2018 03:00:00 GMT+0300')
-  })
-
-})
+    expect(getWeekDay('Вт', 1).toString()).toEqual('Tue Nov 20 2018 03:00:00 GMT+0300');
+  });
+});
 
 describe('Сейчас нечетная неделя', () => {
   beforeEach(() => {
-    tk.travel(new Date('2018-11-07T00:00:00.000Z')) // 7 Oct, Wed, Odd week
-  })
+    tk.travel(new Date('2018-11-07T00:00:00.000Z')); // 7 Oct, Wed, Odd week
+  });
 
   afterAll(() => {
-    tk.reset()
-  })
+    tk.reset();
+  });
 
   it('нужен ближайший вт на четной (день до)', () => {
-    expect(getWeekDay('Вт', 0).toString()).toEqual('Tue Nov 13 2018 03:00:00 GMT+0300')
-  })
+    expect(getWeekDay('Вт', 0).toString()).toEqual('Tue Nov 13 2018 03:00:00 GMT+0300');
+  });
 
   it('нужен ближайший вт на нечетной (день до)', () => {
-    expect(getWeekDay('Вт', 1).toString()).toEqual('Tue Nov 06 2018 03:00:00 GMT+0300')
-  })
-})
+    expect(getWeekDay('Вт', 1).toString()).toEqual('Tue Nov 06 2018 03:00:00 GMT+0300');
+  });
+});
